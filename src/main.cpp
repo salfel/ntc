86a8b2fdf48047bfb2a9c6c957b35005675287a2
Felix Salcher <dev.felix.salcher@gmail.com>
2024-12-04 09:20:01 +0100

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
 #include <Arduino.h>
-
-float calcTemp(int resistance);
-
-float calibrateTemp(float temp);
+#include <math.h>
 
 void setup() {
     pinMode(A4, INPUT);
-    Serial.begin(9600);
+    pinMode(3, OUTPUT);
+    Serial.begin(19200);
+
+    digitalWrite(3, HIGH);
 }
 
-const float BETA = 3977;
-const float T_25 = 298; 
-const float R_25 = 2875;
+const int OUTPUT_PIN = 3;
+
+const float A = -0.0315208;
+const float B = 0.0080451;
+const float C = -0.00006335;
+
 const float STEP = 5.0 / 1024;
 const uint16_t RESISTOR = 2700;
+
+const float UPPER_TRESHHOLD = 27;
+const float LOWER_TRESHHOLD = 26;
 
 void loop() {
     static unsigned long value = 0;
@@ -22,25 +28,21 @@ void loop() {
     static int measured = false; 
 
     uint16_t measurement = analogRead(A4);
-    float voltage = STEP * measurement;
-    float current =  (5 - voltage) / RESISTOR;
-    int temp = (calcTemp(voltage / current) - 273.15) * 100;
-    count++;
-    value += temp;
+    float voltage = 5.0 - (measurement * STEP);
 
-    if (millis() - lastMillis > 5000 && !measured) {
-        Serial.println(calibrateTemp(value / 100.0 / count));
-        Serial.println(count);
-        measured = true;
+    float resistance = (5.0 / voltage) * RESISTOR - RESISTOR;
+    float temperature = 1 / (A + B * log(resistance) + C * pow(log(resistance), 3)) - 273.15;
+
+    if (millis() - lastMillis > 500) {
+        Serial.print(">temperature:");
+        Serial.println(temperature);
+
+        lastMillis = millis();
     }
-}
 
-float calcTemp(int resistance) {
-    float value =  1/T_25 + 1/BETA * log10(resistance / R_25);
-
-    return 1/value;
-}
-
-float calibrateTemp(float temp) {
-    return 2.673557 * temp - 44.290;
+    if (temperature > UPPER_TRESHHOLD) {
+        digitalWrite(3, LOW);
+    } else if (temperature < LOWER_TRESHHOLD){
+        digitalWrite(3, HIGH);
+    }
 }
